3f913774cee371dc9ea077e8f7474b418e2ebc22
Simon Hafner <hafnersimon@gmail.com>
2010-01-09 15:59:18 +0100

    deleted test for trunk

--- test/suite/lib/tree/trunk.rb
+++ test/suite/lib/tree/trunk.rb
@@ -2,14 +2,7 @@ BareTest.suite "CRUDtree" do
 
   suite "tree" do
 
-    suite "trunk" do
-
-      assert "initialize" do
-        trunk = CRUDtree::Trunk.new(object = Object)
-        trunk.klass == object && trunk.stems == []
-      end
-
-    end
+    suite "trunk"
 
   end
 
