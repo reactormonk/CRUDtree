301ee1ffd5addce7bb84b02648b579b836edbb89
Simon Hafner <hafnersimon@gmail.com>
2010-01-09 14:42:21 +0100

    rr doesn't work with 1.9 :-(

--- test/setup.rb
+++ test/setup.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift(File.expand_path("#{__FILE__}/../../lib")) # Add PROJECT/lib to $LOAD_PATH
 require 'crudtree'
-require '/home/freak/coding/ruby/git-sources/baretest/lib/baretest/rr'
+require 'baretest/mocha'
 include CRUDtree
