ae319773a6fdccca7470d6216879bf866275ae3e
Simon Hafner <hafnersimon@gmail.com>
2010-01-10 22:11:22 +0100

    added an example resource helper

---
+++ lib/crudtree/helper.rb
@@ -0,0 +1,17 @@
+module CRUDtree
+  module Interface
+    module Helper
+      def resource(params, &block)
+        stem = @trunk.add_stem(params, &block)
+        stem.collection(call: :index, rest: :get)
+        stem.member(call: :show, rest: :get)
+        stem.member(call: :new, rest: :get)
+        stem.member(call: :create, rest: :post)
+        stem.member(call: :edit, rest: :get)
+        stem.member(call: :update, rest: :put)
+        stem.member(call: :delete, rest: :get)
+        stem.member(call: :destroy, rest: :delete)
+      end
+    end
+  end
+end
