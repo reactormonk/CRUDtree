0f1c794c5c22027e41fbc00c05462aa282efe75d
Simon Hafner <hafnersimon@gmail.com>
2010-01-23 17:55:53 +0100

    updated helper

--- lib/crudtree/helper.rb
+++ lib/crudtree/helper.rb
@@ -2,15 +2,15 @@ module CRUDtree
   module Interface
     module Helper
       def resource(params, &block)
-        node = @master.node(params, &block)
-        node.collection(call: :index, rest: :get)
-        node.member(call: :show, rest: :get)
-        node.member(call: :new, rest: :get)
-        node.member(call: :create, rest: :post)
-        node.member(call: :edit, rest: :get)
-        node.member(call: :update, rest: :put)
-        node.member(call: :delete, rest: :get)
-        node.member(call: :destroy, rest: :delete)
+        resource_node = node(params, &block)
+        resource_node.collection(call: :index, rest: :get)
+        resource_node.collection(call: :new, rest: :get)
+        resource_node.collection(call: :create, rest: :post, path: "")
+        resource_node.member(call: :show, rest: :get)
+        resource_node.member(call: :edit, rest: :get)
+        resource_node.member(call: :update, rest: :put, path: "")
+        resource_node.member(call: :delete, rest: :get)
+        resource_node.member(call: :destroy, rest: :delete, path: "")
       end
     end
   end
